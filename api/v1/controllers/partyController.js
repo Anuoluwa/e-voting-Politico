0b8be82493d5392b25c71978e9dc1f9967d25760
Anu' Apiti <anuoluwa.apiti@gmail.com>
2019-01-27 17:11:08 +0100

    [Feature #163336788] Create method to edit a specific party [finishes #163336788]

--- api/v1/controllers/partyController.js
+++ api/v1/controllers/partyController.js
@@ -33,6 +33,26 @@ class Parties {
     parties.push(newParty);
     res.status(201).json({ message: 'party was created successfully', data: parties });
   }
+
+
+  static async editParty(req, res) {
+    try {
+      const partyId = parseInt(req.params.id, 10);
+      const party = await parties.filter(item => item.partyId == partyId)[0];
+      if (!party) {
+        return res.status(404).json({ message: 'Party does not exist!' });
+      }
+      const index = parties.indexOf(party);
+      const keys = Object.keys(req.body);
+      keys.forEach((key) => {
+        party[key] = req.body[key];
+      });
+      parties[index] = party;
+      res.status(202).json({ message: 'Party updated successfully!', data: parties[index] });
+    } catch (err) {
+      res.status(500).json({ message: 'Sorry about that, not available', err });
+    }
+  }
 }
 
 export default Parties;
