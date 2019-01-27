4c280e6d463ec977352f3e6d2dcdbcb0c8891d80
Anu' Apiti <anuoluwa.apiti@gmail.com>
2019-01-27 21:22:51 +0100

    2

--- api/v1/routes/index.js
+++ api/v1/routes/index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import Party from '../controllers/partyController';
+import Office from '../controllers/officeController';
 import Validator from '../middlewares/partyValidator';
 
 
@@ -11,5 +12,8 @@ router.get('/parties/:id', Validator.validateId, Party.getOneParty);
 router.post('/parties', Party.createParty);
 router.put('/parties/:id', Validator.validateId, Party.editParty);
 router.delete('/parties/:id', Validator.validateId, Party.deleteParty);
+router.get('/offices', Office.getOffices);
+
+
 
 export default router;
