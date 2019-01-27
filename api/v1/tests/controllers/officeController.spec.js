a944b2058d3f73853e8c4ab6243c14ec1270d331
Anu' Apiti <anuoluwa.apiti@gmail.com>
2019-01-27 22:05:27 +0100

    [Feature #163336820] Add test cases for the GET ../offices/id

--- api/v1/tests/controllers/officeController.spec.js
+++ api/v1/tests/controllers/officeController.spec.js
@@ -29,4 +29,30 @@ describe('Test suite for offices endpoint controller', () => {
         });
     });
   });
+  describe('GET /offices/:id', () => {
+    it('should be an object with keys and values that are not null', (done) => {
+      request(app)
+        .get('/api/v1/offices/1')
+        .set('Accept', 'application/json')
+        .expect(200)
+        .end((err, res) => {
+          expect(res.body.officeId).to.not.equal(null);
+          expect(res.body.type).to.not.equal(null);
+          expect(res.body.name).to.not.equal(null);
+          expect(res.body.year).to.not.equal(null);
+          expect(res.body.createdOn).to.not.equal(null);
+          done();
+        });
+    });
+    it('should be an object with keys and values', (done) => {
+      request(app)
+        .get('/api/v1/offices/1')
+        .set('Accept', 'application/json')
+        .expect(200)
+        .end((err, res) => {
+          expect(res.body).to.be.a('object');
+          done();
+        });
+    });
+  });
 });
