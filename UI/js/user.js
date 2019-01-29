1e1d3e4a520c0d36cce98985c26f107ad65f520a
Anu' Apiti <anuoluwa.apiti@gmail.com>
2019-01-29 16:08:11 +0100

    [Feature #163545933] Implement feedback received on user page and view

--- UI/js/user.js
+++ UI/js/user.js
@@ -4,6 +4,9 @@ const officeLink = document.getElementById('office-link');
 const createParty = document.getElementById('createparty');
 const createOffice = document.getElementById('createoffice');
 const allParties = document.getElementById('allparties');
+const offices = document.getElementById('offices');
+const officesDetails = document.getElementById('offices-details');
+
 
 partyLink.addEventListener('click', () => {
   document.querySelector('.active').className = '';
@@ -18,4 +21,9 @@ officeLink.addEventListener('click', () => {
 parties.addEventListener('click', () => {
   document.querySelector('.active').className = '';
   allParties.className = 'active';
+});
+
+offices.addEventListener('click', () => {
+  document.querySelector('.active').className = '';
+  officesDetails.className = 'active';
 });
