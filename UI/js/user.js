ec54a9733aa9b25c0880ac3dd4bc4c90e6752b72
Anu' Apiti <anuoluwa.apiti@gmail.com>
2019-01-29 10:03:23 +0100

    [Feature #163545933] Implement feedback received on admin dashboards

---
+++ UI/js/user.js
@@ -0,0 +1,21 @@
+const parties = document.getElementById('parties');
+const partyLink = document.getElementById('party-link');
+const officeLink = document.getElementById('office-link');
+const createParty = document.getElementById('createparty');
+const createOffice = document.getElementById('createoffice');
+const allParties = document.getElementById('allparties');
+
+partyLink.addEventListener('click', () => {
+  document.querySelector('.active').className = '';
+  createParty.className = 'active';
+});
+
+officeLink.addEventListener('click', () => {
+  document.querySelector('.active').className = '';
+  createOffice.className = 'active';
+});
+
+parties.addEventListener('click', () => {
+  document.querySelector('.active').className = '';
+  allParties.className = 'active';
+});
