9859614a0dbcd6d4bb36f9fd16218be1a02270aa
Anu' Apiti <anuoluwa.apiti@gmail.com>
2019-02-01 12:28:40 +0100

    Feature(Input Validation) : - Validate PATCH Method - Fix validation inputs for name fields

--- api/v1/middlewares/inputValidator.js
+++ api/v1/middlewares/inputValidator.js
@@ -10,31 +10,27 @@ class ValidateInput {
       return res.status(400)
         .json({ error: 'name input should be a string' });
     }
-    if (name instanceof String) {
-      return res.status(400)
-        .json({ error: 'name input should be a string' });
+    if (!name) {
+      return res.status(400).json({ error: 'name must be a string!' })
+        .end();
     }
     if (name.length === '') {
       return res.status(400)
         .json({ error: 'name must not be empty' });
     }
-    if (!(/[^a-zA-Z]/.test(name))) {
-      return res.status(400)
-        .json({ error: 'name input should be a string without number or special characters' });
-    }
-    if (!name) {
-      return res.status(400).json({ error: 'name must be a string!' })
-        .end();
-    }
     if (name.length < 8) {
       return res.status(400).json(
-        { error: 'name must be a string with minimum 20 characters' },
+        { error: 'name must be a string with minimum 8 characters' },
       )
         .end();
     }
     if (name.length > 100) {
-      return res.status(400).json({ error: 'name must be a string with maximum 200 characters' })
+      return res.status(400).json({ error: 'name must be a string with maximum 100 characters' })
         .end();
+    }
+    if (!(/^[A-Za-z ]+$/.test(name))) {
+      return res.status(400)
+        .json({ error: 'name input should be a string without number or special characters' });
     }
     if (!hqAddress) {
       return res.status(400).json('hqAddress must be a string')
@@ -55,11 +51,11 @@ class ValidateInput {
     }
 
     if (hqAddress.length < 7) {
-      return res.status(400).json({ messge: 'hqAddress must be a string with minimum 20 characters' })
+      return res.status(400).json({ messge: 'hqAddress must be a string with minimum 7 characters' })
         .end();
     }
     if (hqAddress.length > 50) {
-      return res.status(400).json({ error: 'hqAddress must be a string with maximum 20 character ' })
+      return res.status(400).json({ error: 'hqAddress must be a string with maximum 50 character ' })
         .end();
     }
 
@@ -80,11 +76,11 @@ class ValidateInput {
         .end();
     }
     if (logoUrl.length < 8) {
-      return res.status(400).json({ error: 'logUrl must be a string with minimum 20 characters' })
+      return res.status(400).json({ error: 'logUrl must be a string with minimum 8 characters' })
         .end();
     }
     if (logoUrl.length > 70) {
-      return res.status(400).json({ error: 'logUrl must be a string with maximum 20 character ' })
+      return res.status(400).json({ error: 'logUrl must be a string with maximum 70 character ' })
         .end();
     }
     next();
@@ -100,31 +96,27 @@ class ValidateInput {
       return res.status(400)
         .json({ error: 'type input should be a string' });
     }
-    if (type instanceof String) {
-      return res.status(400)
-        .json({ error: 'type input should be a string' });
-    }
     if (type.length === '') {
       return res.status(400)
         .json({ error: 'type must not be empty' });
     }
     if (!type) {
-      return res.status(400).json('type must be a string')
+      return res.status(400).json('type must be a string should not empty')
         .end();
     }
-    if (type.length < 7) {
-      return res.status(400).json({ messge: 'type must be a string with minimum 7 characters' })
+    if (type.length < 6) {
+      return res.status(400).json({ messge: 'type must be a string with minimum 6 characters' })
         .end();
     }
     if (type.length > 50) {
       return res.status(400).json({ error: 'type must be a string with maximum 50 character ' })
         .end();
     }
-    if (typeof name !== 'string') {
+    if (!(/^[A-Za-z ]+$/.test(type))) {
       return res.status(400)
-        .json({ error: 'name input should be a string' });
+        .json({ error: 'name input should be a string without number or special characters' });
     }
-    if (name instanceof String) {
+    if (typeof name !== 'string') {
       return res.status(400)
         .json({ error: 'name input should be a string' });
     }
@@ -132,12 +124,8 @@ class ValidateInput {
       return res.status(400)
         .json({ error: 'name must not be empty' });
     }
-    if (!/[^a-zA-Z]/.test(name)) {
-      return res.status(400)
-        .json({ error: 'name input should be a string without number or special characters' });
-    }
     if (!name) {
-      return res.status(400).json({ error: 'name must be a string!' })
+      return res.status(400).json({ error: 'name must be a string and should not be empty!' })
         .end();
     }
     if (name.length < 8) {
@@ -149,6 +137,10 @@ class ValidateInput {
     if (name.length > 100) {
       return res.status(400).json({ error: 'name must be a string with maximum 100 characters' })
         .end();
+    }
+    if (!(/^[A-Za-z ]+$/.test(name))) {
+      return res.status(400)
+        .json({ error: 'name input should be a string without number or special characters' });
     }
     next();
   }
--- api/v1/routes/index.js
+++ api/v1/routes/index.js
@@ -10,7 +10,7 @@ router.get('/', (req, res) => res.json('Successful!, Welcome to Politico API v1!
 router.get('/parties', Party.getParties);
 router.get('/parties/:id', Validator.validateId, Party.getOneParty);
 router.post('/parties', Validator.partyInput, Party.createParty);
-router.patch('/parties/:id', Validator.validateId, Party.editParty);
+router.patch('/parties/:id', Validator.validateId, Validator.partyInput, Party.editParty);
 router.delete('/parties/:id', Validator.validateId, Party.deleteParty);
 router.get('/offices', Office.getOffices);
 router.get('/offices/:id', Validator.validateId, Office.getOneOffice);
