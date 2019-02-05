60931400d2e86bf84b968b85f5ad018fcba96b50
Anu' Apiti <anuoluwa.apiti@gmail.com>
2019-02-05 09:18:16 +0100

    [Fefature Create new office]: implement endpoint to create new office - Implement POST /api/v2/offices to add new party [Delivers #163639495]

--- api/v2/routes/index.js
+++ api/v2/routes/index.js
@@ -15,14 +15,15 @@ router.get('/', (req, res) => res.json('Successful!, Welcome to Politico API v1!
 
 router.post('/auth/signup', authValidation.signup, Auth.signUp);
 router.post('/auth/login', authValidation.login, Auth.login);
-router.post('/parties', verifyToken, verifyAdmin, Party.createParty);
+router.post('/parties', verifyToken, Party.createParty);
 router.get('/parties', verifyToken, Party.getAllParties);
+router.post('/offices', verifyToken, Office.createOffice);
+
 // router.get('/parties/:id', verifyToken, verifyAdmin, Party.getOneParty);
 // router.patch('/parties/:id', verifyToken, verifyAdmin, Party.editParty);
 // router.delete('/parties/:id', verifyToken, verifyAdmin, Party.deleteParty);
 // router.get('/offices', verifyToken, verifyAdmin, Office.getOffices);
 // router.get('/offices/:id', verifyToken, verifyAdmin, Office.getOneOffice);
-// router.post('/offices', verifyToken, verifyAdmin, Office.createOffice);
 router.use('*', (req, res) => res.json('Route does not exist'));
 
 
