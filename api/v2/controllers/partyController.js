35f9b199b5db46af614d7cdc6f31601cd698162d
Anu' Apiti <anuoluwa.apiti@gmail.com>
2019-02-05 06:34:05 +0100

    Feat(Endpoint to create party): Implement POST api/v2/parties to add new party - No validations yet [Delivers #163639501]

--- api/v2/controllers/partyController.js
+++ api/v2/controllers/partyController.js
@@ -1,13 +1,59 @@
+import db from '../config/connection';
+import { createParty, findParty } from '../models/queries';
+/**
+ * Creates a new PartyController.
+ * @class
+ * @classdesc PartyController has two static methods.
+ */
 class Parties {
-  static async getParties(req, res) {
-    res.json({ message: 'logic to get all the parties' });
+  /**
+ * @method createParty
+ * @static
+ * @description this takes of POST party method
+ * @constructor none
+ * @param {object} req req object
+ * @param {object} res res object
+ * @returns {Object} status 409 if you already created a party with the same name
+ * @returns {Object} status 200 Party created successfully
+ * @returns {Object} status 500 for server error
+ */
+  static async createParty(req, res) {
+    try {
+      const userId = req.user.id;
+      const {
+        partyName, hqAddress, logoUrl,
+      } = req.body;
+      const partyNameExists = await db.query(findParty(partyName));
+      if (partyNameExists.rowCount > 0) {
+        return res.status(409).json({
+          status: 409,
+          error: 'A party is registered with this name',
+        });
+      }
+      const newParty = {
+        partyName, hqAddress, logoUrl, userId,
+      };
+      const addParty = await db.query(createParty(newParty));
+      if (addParty.rowCount > 0) {
+        return res.status(201).json({
+          status: 200,
+          party: addParty.rows,
+        });
+      }
+    } catch (error) {
+      console.log({ message: `${error}` });
+      return res.status(500).json({
+        status: 500,
+        error: 'Sorry, something went wrong, try again!',
+      });
+    }
   }
 
   static async getOneParty(req, res) {
     res.json({ message: 'logic to get all the one party' });
   }
 
-  static createParty(req, res) {
+  static getAllParties(req, res) {
     res.json({ message: 'logic to create party goes here' });
   }
 
