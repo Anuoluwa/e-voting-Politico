5cec6003c0322dbbc76c8ce986fed7a6facd4378
Anu' Apiti <anuoluwa.apiti@gmail.com>
2019-02-06 12:33:38 +0100

    Add logic for voters controller for users to vote

---
+++ api/v2/controllers/voteController.js
@@ -0,0 +1,54 @@
+import db from '../config/connection';
+import { createVote, checkOffice, checkContestant } from '../models/queries';
+/**
+ * Creates a new VoteController.
+ * @class
+ * @classdesc VoteController has static methods.
+ */
+class Votes {
+  static async voteCandidate(req, res) {
+    try {
+      const createdBy = req.user.id;
+      const office = Number(req.body.office, 10);
+      const candidate = Number(req.body.candidate, 10);
+      const getOffice = await db.query(checkOffice(office));
+      if (getOffice.rowCount === 0) {
+        return res.status(404).json({
+          status: 404,
+          error: 'office does not exist',
+        });
+      }
+      const contestantExists = await db.query(checkContestant(candidate));
+      if (contestantExists.rowCount === 0) {
+        return res.status(404).json({
+          status: 404,
+          error: 'candidate does not exist',
+        });
+      }
+      const newVote = {
+        createdBy, office, candidate,
+      };
+      const vote = await db.query(createVote(newVote));
+      if (vote.rowCount > 0) {
+        return res.status(201).json({
+          status: 201,
+          data: [vote.rows],
+        });
+      }
+    } catch (error) {
+      console.log({ message: `${error}` });
+      if (error.constraint.includes('votes_pkey')) {
+        return res.status(409).json({
+          status: 409,
+          error: 'Sorry, you cannot vote for the same candidate twice',
+        });
+      }
+      return res.status(500).json({
+        status: 500,
+        error: 'Sorry, something went wrong, try again!',
+      });
+    }
+  }
+}
+
+export default Votes;
