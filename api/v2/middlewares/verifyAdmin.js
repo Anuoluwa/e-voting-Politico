8efdb706f54b67f93d7db95864bb16663006296a
Anu' Apiti <anuoluwa.apiti@gmail.com>
2019-02-19 22:03:20 +0100

    [Fixes: Admin Token]: Fix admin token for admin protected endpoint

--- api/v2/middlewares/verifyAdmin.js
+++ api/v2/middlewares/verifyAdmin.js
@@ -6,8 +6,9 @@
 /* @returns {Object} status 401 if is a user
 */
 const verifyAdmin = async (req, res, next) => {
+  console.log('typeofIsAdmin_verify_admin', typeof (req.user.admin));
   try {
-    if (req.user.isAdmin === false) {
+    if (req.user.isAdmin === false || req.user.isAdmin === 'undefined') {
       return res.status(403).json({ status: 403, error: 'Unauthorized, ACESSS DENIED' });
     }
     if (req.user.isAdmin === true) {
